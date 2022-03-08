3be89ae2c60853b91c3f9a5f32ceabf3554e7e55
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 20:16:55 +0100

    Iteration#6

--- index.js
+++ index.js
@@ -83,3 +83,32 @@ function averageWord(param) {
 }
 averageWord(mixedElements);
 
+// Iteration 6
+
+const duplicates = [
+    "sushi",
+    "pizza",
+    "burger",
+    "potatoe",
+    "pasta",
+    "ice-cream",
+    "pizza",
+    "chicken",
+    "onion rings",
+    "pasta",
+    "soda",
+  ];
+  
+  function removeDuplicates(param) {
+    
+    let unique = [];
+    
+    for (let i = 0; i < param.length; i++) {
+      if (!unique.includes(param[i])) {
+        unique.push(param[i]);
+      }
+    }
+    return console.log(unique);
+  }
+  
+  removeDuplicates(duplicates);
