88e561310cfa4542dcab1f678e451ec8e83cde3b
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 19:53:32 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -9,5 +9,29 @@ if (numOne > numTwo) {
     return numTwo;
     }
 }
-    
+
+// Iteration 2
+
+// Iteration 2
+
+const avengers = [
+    "Hulk",
+    "Thor",
+    "IronMan",
+    "Captain A.",
+    "Spiderman",
+    "Captain M.",
+  ];
+  function findLongestWord(param) {
+    let longestWord = param[0];
+    for (let i = 1; i < param.length; i++) {
+      if (param[i].length > longestWord.length) {
+        longestWord = param[i];
+      }
+    }
+    return console.log(longestWord);
+  }
+  findLongestWord(avengers);
+ 
+ 
     
