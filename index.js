21c67712f3a5a50846269abe863a35c572c86540
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 20:14:33 +0100

    Iteration#5

--- index.js
+++ index.js
@@ -61,9 +61,25 @@ function average(array) {
     for (let i = 0; i < array.length; i++) {
     sum += array[i];
     }
-    
+
 return console.log(Math.round(sum / array.length));
 }
 
 average(nums);
+
+// Iteration 5
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param) {
+  
+   let sum = 0;
+   for (let i = 0; i < param.length; i++) {
+     if (typeof param[i] === "string") {
+       sum += param[i].length;
+     }
+   }
+   return console.log(sum/param.length); 
+}
+averageWord(mixedElements);
 
