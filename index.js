d5c78f8e389068c1cdd4fdc8769e61787eecfc41
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 20:29:52 +0100

    Iteration#7

--- index.js
+++ index.js
@@ -112,3 +112,30 @@ const duplicates = [
   }
   
   removeDuplicates(duplicates);
+
+  // Iteration 7
+
+  const nameFinder = [
+    "Peter",
+    "Steve",
+    "Tony",
+    "Natasha",
+    "Clint",
+    "Logan",
+    "Xabier",
+    "Bruce",
+    "Peggy",
+    "Jessica",
+    "Marc",
+  ];
+  
+  function finderName(param, value) {
+    for (let i = 0; i < param.length; i++) {
+      if (param[i] === value) {
+        return console.log(true, i);
+      }
+    }
+    return console.log(false);
+  }
+  
+  finderName(nameFinder, "Peggy");
